b9d818100e1de998aaa1bf09d1357a8831f6b3ab
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-12-23 07:51:33 +0530

    updated

--- Client/src/components/api/index.js
+++ Client/src/components/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/posts";
+const url = "https://server-phi-six.vercel.app/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
